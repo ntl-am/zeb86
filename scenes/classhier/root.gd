dcc6a07b88be34b1cd319608c11bec60346912aa
matheusanmo <andrade94kaizen@gmail.com>
2025-06-09 21:12:55 -0300

    first commit

---
+++ scenes/classhier/root.gd
@@ -0,0 +1,11 @@
+class_name Root
+
+extends Node
+
+var turn = 1
+var turn_phase = "PC"
+var day = 1
+var time = Vector2i(14, 20)
+var ingametime = 12 # minutos
+var paused = false
+var gameover = false
