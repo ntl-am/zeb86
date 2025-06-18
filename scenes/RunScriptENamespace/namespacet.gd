be168bc48276444929ad79e1db897f0675b26bcb
matheusanmo <andrade94kaizen@gmail.com>
2025-06-18 05:02:50 -0300

    ablublu

--- scenes/RunScriptENamespace/namespacet.gd
+++ scenes/RunScriptENamespace/namespacet.gd
@@ -4,3 +4,19 @@ static func cumprimentar():
 	print("oi eu vim do nsteste!!!!")
 
 const cachorrinho = "ZEKINHA"
+
+static func pathcost(origin : Vector2i, target : Vector2i) -> int:
+	var dv = origin - target
+	var dc = absi(dv.x)
+	var dl = absi(dv.y)
+	if dc < dl: #forcing bresenhaam like octant condition
+		var dc_ = dc
+		dc = dl
+		dl = dc_
+	if dv == Vector2i.ZERO: return 0
+	if dc == 0: return dl
+	if dl == 0: return dc
+	var diags = dc - dl
+	var orthos = dc - diags
+	var diagscost = diags + floori(diags/2)
+	return orthos + diagscost
--- scenes/RunScriptENamespace/new_script.gd
+++ scenes/RunScriptENamespace/new_script.gd
@@ -1,7 +1,42 @@
 @tool
 extends EditorScript
 
+func cross(xs : Array, ys : Array) -> Array:
+	var ret : Array = []
+	for x in xs:
+		for y in ys:
+			ret.append([x,y])
+	return ret
+
+func neighbours(o : Vector2i) -> Array:
+	var cols = range(o.x-1, o.x+2)
+	var lines = range(o.y-1, o.y+2)
+	var ret = []
+	for c in cols:
+		for l in lines:
+			ret.append(Vector2i(c,l))
+	return ret
+
+class TreeNode extends Node:
+	var parent
+	var coords : Vector2i
+	var cost : int
+	func _init(n:StringName, c:Vector2i, parent):
+		parent = parent
+		name = n
+		coords = c
+		cost = 0 if parent == null else NSTest.pathcost(parent.coords, c)
+
 func _run():
-	print("oirrr")
-	NSTest.cumprimentar()
-	print(NSTest.cachorrinho)
+	var c : Vector2i = Vector2i(7,7)
+	var o = TreeNode.new(&"origin",Vector2i(7,7), null)
+	var ons = neighbours(o.coords)
+	var layer = 1
+	var sibling = 1
+	for n in ons:
+		var cost =999
+		var nodename = StringName("<%d, %d> V2i(%s) cost: %d" % [layer, sibling, n, cost])
+		sibling += 1
+		o.add_child(TreeNode.new(nodename, n, o))
+	o.print_tree_pretty()
+	
